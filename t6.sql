32bc71fcbc0eaaaa14e7561d1a7640e535da3074
Akhil Ravindran <imakhilravindran@gmail.com>
2020-12-11 14:12:08 +0530

    Change datatype t6.sql 11122020

--- t6.sql
+++ t6.sql
@@ -1,18 +1,18 @@
 #a
 CREATE TABLE Store (
-    `order_no` INTEGER PRIMARY KEY AUTO_INCREMENT,
+	`order_no` INTEGER PRIMARY KEY AUTO_INCREMENT,
     `code` VARCHAR(20) NOT NULL,
     `item` VARCHAR(30) NOT NULL,
-    `quantity` INTEGER DEFAULT 1,
-    `price` INTEGER NOT NULL,
-    `discount` INTEGER DEFAULT 0,
-    `mrp` INTEGER NOT NULL
+    `quantity` INTEGER UNSIGNED DEFAULT 1,
+    `price` DECIMAL(10,2) NOT NULL,
+    `discount` DECIMAL(2,2) DEFAULT 0,
+    `mrp` DECIMAL(10,2) NOT NULL
 );
 
 
 #b
 INSERT INTO Store (`code`, `item`, `quantity`, `price`, `discount`, `mrp`) 
-VALUES ("PST", "PASTE", 2, 20, 0, 20),("BOK", "NOTEBOOK", 10, 45, 5, 50);
+VALUES ("PST", "PASTE", 2, 20, 0, 20),("BOK", "NOTEBOOK", 10, 45, 0.1, 50);
 
 #c
 SELECT * FROM Store;
